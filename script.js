c9b56ea8fbb513ef6c6d41b45f841e4bf84b7448
Alphonse Johnson <a.joh26@gmail.com>
2023-03-27 21:52:31 -0500

    deleted develop folder

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 
 var currDay = $("#currentDay")
-var timeBar = $("#id")
-
-var work = $("#hour-8")
-
-
 
 // Adds current time at the headers
 
@@ -39,6 +30,7 @@ function timeCompare() {
 
 
       // console.log(parseInt(barId))
+// checks and compares time to set the give space to past class
 
       if (parseInt(barId) < xTime) {
         $(this).removeClass("future")
@@ -47,7 +39,7 @@ function timeCompare() {
 
         // console.log($(this))
       }
-
+// checks and compares time to set the give space to present class
       else if (barId === xTime) {
 
         $(this).addClass("present");
@@ -55,7 +47,7 @@ function timeCompare() {
         $(this).removeClass("future");
 
       }
-
+// Sets remaining entry spaces to future class if other conditions are false
       else {
         $(this).addClass("future")
         $(this).removeClass("present")
@@ -65,12 +57,14 @@ function timeCompare() {
     )
     // if(currTime)
     // timeBar.attr("class", "future
-  }, 2000);
+  }, 1000);
 }
 
 timeCompare();
 
 
+// creates a button listener to hear clicks with the save button is press
+// Button record values to the given space and also saves value to the local storage
 
 $(document).ready(function () {
   $(".saveBtn").click(function () {
@@ -91,34 +85,28 @@ $(document).ready(function () {
   })
 
 
+  // function created to grab local storage values and set them corresponding space desginated by given ID
   $(function () {
 
+
+    $("#hour-7 textarea").val(localStorage.getItem("hour-7"))
     $("#hour-8 textarea").val(localStorage.getItem("hour-8"))
     $("#hour-9 textarea").val(localStorage.getItem("hour-9"))
     $("#hour-10 textarea").val(localStorage.getItem("hour-10"))
-  }
+    $("#hour-11 textarea").val(localStorage.getItem("hour-11"))
+    $("#hour-12 textarea").val(localStorage.getItem("hour-12"))
+    $("#hour-13 textarea").val(localStorage.getItem("hour-13"))
+    $("#hour-14 textarea").val(localStorage.getItem("hour-14"))
+    $("#hour-15 textarea").val(localStorage.getItem("hour-15"))
+    $("#hour-16 textarea").val(localStorage.getItem("hour-16"))
+    $("#hour-17 textarea").val(localStorage.getItem("hour-17"))
+    $("#hour-18 textarea").val(localStorage.getItem("hour-18"))
+    $("#hour-19 textarea").val(localStorage.getItem("hour-19"))
 
-  );
 
+  });
 })
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
+
+
